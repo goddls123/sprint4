dec05bf6d9d3a67dbf7ebc00934c4b2938a0b554
Jun <coolch0916@gmail.com>
2024-04-09 09:06:07 +0900

    add env file

--- backend/src/settings.ts
+++ backend/src/settings.ts
@@ -1,4 +1,10 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 export const PORT = process.env.PORT || 3031;
-export const CORS_ALLOWED_ORIGIN = process.env.CORS_ALLOWED_ORIGIN || '';
+export const CORS_ALLOWED_ORIGIN = process.env.CORS_ALLOWED_ORIGIN || "";
+
+export const HOST = process.env.DB_HOST;
+export const DB_USER = process.env.DB_USER;
+export const DATABASE = process.env.DATABASE;
+export const DB_PASSWORD = process.env.DB_PASS;
+export const DB_PORT = process.env.DB_PORT;
--- backend/src/utils/mysql.ts
+++ backend/src/utils/mysql.ts
@@ -1,12 +1,13 @@
-import { createConnection } from 'mysql2';
+import { createConnection } from "mysql2";
+import { DATABASE, DB_PASSWORD, DB_PORT, DB_USER, HOST } from "../settings";
 
 const connection = createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'prgms_notes',
+  host: HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DATABASE,
   dateStrings: true,
-  port: 30036,
+  port: Number(DB_PORT),
 });
 
 export default connection;
